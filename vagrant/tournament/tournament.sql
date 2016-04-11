9dc95af5cdcd99581e36972346c95c1d3eac5848
Darold So <daroldso@gmail.com>
2016-04-11 13:49:30 +0800

    Add create Players, Matches table script

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,13 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE IF NOT EXISTS players (
+    id serial primary key,
+    name text
+);
 
+CREATE TABLE IF NOT EXISTS matches (
+    player1_id int references players(id),
+    player2_id int references players(id),
+    winner int references players(id)
+);
