cd2aed335a84b01ba291fc4cebbe137364f01e87
Darold So <daroldso@gmail.com>
2016-04-11 14:43:23 +0800

    Refactor views

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -24,30 +24,13 @@ CREATE TABLE IF NOT EXISTS matches (
     winner int references players(id)
 );
 
+CREATE VIEW total_matches_of_players AS SELECT id, count(matches.player1_id) AS matches FROM players LEFT JOIN matches ON matches.player1_id = players.id OR matches.player2_id = players.id GROUP BY players.id;
+
+CREATE VIEW total_wins_of_players AS SELECT id, count(matches.winner) AS wins FROM players LEFT JOIN matches ON matches.winner = players.id GROUP BY players.id;
+
 CREATE VIEW standing AS SELECT players.id, name, match2.wins, match1.matches from players
-LEFT JOIN (SELECT id, count(matches.player1_id) AS matches FROM players LEFT JOIN matches ON matches.player1_id = players.id OR matches.player2_id = players.id GROUP BY players.id) as match1
+LEFT JOIN (SELECT * from total_matches_of_players) as match1
 ON players.id = match1.id
-LEFT JOIN (SELECT id, count(matches.winner) AS wins FROM players LEFT JOIN matches ON matches.winner = players.id GROUP BY players.id) as match2
+LEFT JOIN (SELECT * from total_wins_of_players) as match2
 ON players.id = match2.id
 ORDER BY wins DESC;
-
-INSERT INTO players (name) VALUES ('Darold So');
-INSERT INTO players (name) VALUES ('Esther Wong');
-INSERT INTO players (name) VALUES ('Alvin Ng');
-INSERT INTO players (name) VALUES ('Chun Sing');
-INSERT INTO players (name) VALUES ('Chun Ka');
-INSERT INTO players (name) VALUES ('Leo Chan');
-INSERT INTO players (name) VALUES ('Finnian Lui');
-INSERT INTO players (name) VALUES ('Vincent Chan');
-
-INSERT INTO matches VALUES (1, 2, 1);
-INSERT INTO matches VALUES (3, 4, 3);
-INSERT INTO matches VALUES (5, 6, 5);
-INSERT INTO matches VALUES (7, 8, 8);
-
-INSERT INTO matches VALUES (1, 3, 3);
-INSERT INTO matches VALUES (5, 8, 8);
-INSERT INTO matches VALUES (2, 4, 2);
-INSERT INTO matches VALUES (6, 7, 7);
-
-INSERT INTO matches VALUES (1, 8, 8);
