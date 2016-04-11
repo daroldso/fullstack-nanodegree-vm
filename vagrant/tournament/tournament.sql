423f4e37ac2bcccfed33f073c6ddb2a5d87fe1e2
Darold So <daroldso@gmail.com>
2016-04-11 15:19:43 +0800

    Create view for displaying bye round of each player

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -24,11 +24,13 @@ CREATE TABLE IF NOT EXISTS matches (
     winner int references players(id)
 );
 
-CREATE VIEW total_matches_of_players AS SELECT id, count(matches.player1_id) AS matches FROM players LEFT JOIN matches ON matches.player1_id = players.id OR matches.player2_id = players.id GROUP BY players.id;
+CREATE VIEW total_matches_of_players AS SELECT id, players.name, count(matches.player1_id) AS matches FROM players LEFT JOIN matches ON matches.player1_id = players.id OR matches.player2_id = players.id GROUP BY players.id;
 
-CREATE VIEW total_wins_of_players AS SELECT id, count(matches.winner) AS wins FROM players LEFT JOIN matches ON matches.winner = players.id GROUP BY players.id;
+CREATE VIEW total_wins_of_players AS SELECT id, players.name, count(matches.winner) AS wins FROM players LEFT JOIN matches ON matches.winner = players.id GROUP BY players.id;
 
-CREATE VIEW standing AS SELECT players.id, name, match2.wins, match1.matches from players
+CREATE VIEW bye_round_of_players AS SELECT id, name, count(matches.player1_id) FROM players LEFT JOIN (SELECT player1_id FROM matches WHERE player2_id is NULL) AS matches ON matches.player1_id = players.id GROUP BY players.id;
+
+CREATE VIEW standing AS SELECT players.id, players.name, match2.wins, match1.matches from players
 LEFT JOIN (SELECT * from total_matches_of_players) as match1
 ON players.id = match1.id
 LEFT JOIN (SELECT * from total_wins_of_players) as match2
