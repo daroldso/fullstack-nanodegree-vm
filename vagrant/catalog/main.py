68504f770bba593e047041c5ef5041ffd525e424
Darold So <daroldso@gmail.com>
2016-05-16 13:01:10 +0800

    Fix code style

--- vagrant/catalog/main.py
+++ vagrant/catalog/main.py
@@ -1,12 +1,14 @@
 from functools import wraps
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for,\
+    flash, jsonify, abort
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker, exc
 from database_setup import Genre, Base, Artist, User
 import datetime
 
 from flask import session as login_session
-import string, random
+import string
+import random
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
@@ -27,6 +29,7 @@ NEW_ARTIST_EXPIRY_DURATION = 30
 # Client ID from client_secrets.json
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -76,12 +79,14 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check if the user is already logged in by checking if credentials already stored
+    # Check if the user is already logged in by checking if
+    # credentials already stored
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps(
+            'Current user is already connected.'),
+             200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -90,7 +95,8 @@ def gconnect():
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
-    # use the credential access token to request logged in user info from Google
+    # use the credential access token to request logged in user info
+    # from Google
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
@@ -113,12 +119,13 @@ def gconnect():
     output += '<h1>Welcome, '
     output += login_session['username']
     output += '!</h1>'
-    output += '<img src="'
+    output += '<img class="login-profile-pic" src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " > '
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
+
 
 @app.route('/gdisconnect', methods=['POST'])
 def gdisconnect():
@@ -141,6 +148,7 @@ def gdisconnect():
             json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
+
 
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
@@ -167,7 +175,7 @@ def fbconnect():
     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print "url sent for API access:%s"% url
+    print "url sent for API access:%s" % url
     print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
@@ -197,22 +205,25 @@ def fbconnect():
     output += login_session['username']
 
     output += '!</h1>'
-    output += '<img src="'
+    output += '<img class="login-profile-pic" src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " > '
 
     flash("Now logged in as %s" % login_session['username'])
     return output
+
 
 @app.route('/fbdisconnect')
 def fbdisconnect():
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (
+        facebook_id, access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
+
 
 @app.route('/disconnect')
 def disconnect():
@@ -235,6 +246,7 @@ def disconnect():
         flash("You were not logged in")
         return redirect(url_for('home'))
 
+
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
@@ -256,11 +268,14 @@ def getUserID(email):
     except:
         return None
 
+
 @app.route('/login')
 def showLogin():
-    csrf_token = ''.join(random.choice(string.ascii_letters + string.digits) for x in xrange(32))
+    csrf_token = ''.join(random.choice(
+        string.ascii_letters + string.digits) for x in xrange(32))
     login_session['csrf_token'] = csrf_token
     return render_template('login.html', csrf_token=csrf_token)
+
 
 def login_required(f):
     @wraps(f)
@@ -270,12 +285,14 @@ def login_required(f):
         return f(*args, **kwargs)
     return is_user_logged_in
 
+
 @app.route('/')
 def home():
     genres = session.query(Genre).all()
     # Get the time difference with today and expiry duration we defined earlier
     currentTime = datetime.datetime.today()
-    timeDifference = currentTime - datetime.timedelta(days=NEW_ARTIST_EXPIRY_DURATION)
+    timeDifference = currentTime - datetime.timedelta(
+        days=NEW_ARTIST_EXPIRY_DURATION)
     # Fetch the artists created within expiry duration
     artists = session.query(Artist).filter(
         Artist.created_at > timeDifference).order_by(desc(Artist.created_at))
@@ -285,24 +302,31 @@ def home():
 
     return render_template('home.html', genres=genres, artists=artists)
 
+
 @app.route('/genres/<int:genre_id>/')
 def showGenreArtists(genre_id):
     genres = session.query(Genre).all()
     genre = session.query(Genre).filter_by(id=genre_id).one()
     return render_template('showGenreArtist.html', genres=genres, genre=genre)
 
+
 @app.route('/artists/new', methods=['GET', 'POST'])
 @login_required
 def newArtist():
     genres = session.query(Genre).all()
     if request.method == 'POST':
-        newArtist = Artist(name=request.form['artistName'], biography=request.form['artistBio'], created_at=datetime.datetime.today(), genre_id=request.form['artistGenre'], user_id=login_session['user_id'])
+        newArtist = Artist(name=request.form['artistName'],
+                           biography=request.form['artistBio'],
+                           created_at=datetime.datetime.today(),
+                           genre_id=request.form['artistGenre'],
+                           user_id=login_session['user_id'])
         session.add(newArtist)
         session.commit()
         flash('New Artist %s Successfully Created' % (newArtist.name))
         return redirect(url_for('home'))
     else:
         return render_template('newArtist.html', genres=genres)
+
 
 @app.route('/artists/<int:artist_id>/')
 def showArtist(artist_id):
@@ -312,10 +336,12 @@ def showArtist(artist_id):
         artist = session.query(Artist).filter_by(id=artist_id).one()
     except exc.NoResultFound:
         abort(404)
-    else:
-        return redirect(url_for('home'))
     biography = artist.biography.encode().split('\n')
-    return render_template('showArtist.html', genres=genres, artist=artist, biography=biography)
+    return render_template('showArtist.html',
+                           genres=genres,
+                           artist=artist,
+                           biography=biography)
+
 
 def is_user_authed(uid, sess_id):
     """Tests whether a user is authorized to make CRUD action"""
@@ -323,6 +349,7 @@ def is_user_authed(uid, sess_id):
         abort(403)
     else:
         return True
+
 
 @app.route('/artists/<int:artist_id>/edit/', methods=['GET', 'POST'])
 @login_required
@@ -332,19 +359,23 @@ def editArtist(artist_id):
         editedArtist = session.query(Artist).filter_by(id=artist_id).one()
     except:
         return redirect(url_for('home'))
-    if is_user_authed(editedArtist.user_id, login_session['user_id']) and request.method == 'POST':
+    if is_user_authed(editedArtist.user_id, login_session['user_id']) \
+            and request.method == 'POST':
         if request.form['artistName']:
-            editedArtist.name =request.form['artistName']
+            editedArtist.name = request.form['artistName']
         if request.form['artistBio']:
-            editedArtist.biography =request.form['artistBio']
+            editedArtist.biography = request.form['artistBio']
         if request.form['artistGenre']:
-            editedArtist.genre_id =request.form['artistGenre']
+            editedArtist.genre_id = request.form['artistGenre']
         session.add(editedArtist)
         session.commit()
         flash('Artist Successfully Edited')
         return redirect(url_for('home'))
     else:
-        return render_template('editArtist.html', artist=editedArtist, genres=genres)
+        return render_template('editArtist.html',
+                               artist=editedArtist,
+                               genres=genres)
+
 
 @app.route('/artists/<int:artist_id>/delete/', methods=['GET', 'POST'])
 @login_required
@@ -353,13 +384,15 @@ def deleteArtist(artist_id):
         deletedArtist = session.query(Artist).filter_by(id=artist_id).one()
     except:
         return redirect(url_for('home'))
-    if is_user_authed(deletedArtist.user_id, login_session['user_id']) and request.method == 'POST':
+    if is_user_authed(deletedArtist.user_id, login_session['user_id']) \
+            and request.method == 'POST':
         session.delete(deletedArtist)
         session.commit()
         flash('Artist Successfully Deleted')
         return redirect(url_for('home'))
     else:
         return render_template('deleteArtist.html', artist=deletedArtist)
+
 
 @app.route('/genres.json')
 def genresJSON():
@@ -382,15 +415,18 @@ def genresJSON():
         items.append(serialized)
     return jsonify(genres=items)
 
+
 @app.route('/artists.json')
 def artistsJSON():
     artists = session.query(Artist).all()
     return jsonify(artists=[i.serialize for i in artists])
 
+
 @app.route('/artists/<int:artist_id>/json')
 def artistJSON(artist_id):
     artist = session.query(Artist).filter_by(id=artist_id).one()
     return jsonify(artist.serialize)
+
 
 @app.route('/genres/<int:genre_id>/artists.json')
 def genreArtistsJSON(genre_id):
@@ -399,9 +435,11 @@ def genreArtistsJSON(genre_id):
         genre_id=genre_id).all()
     return jsonify(artists=[i.serialize for i in items])
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(403)
 def page_forbidden(e):
